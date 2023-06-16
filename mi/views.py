9de6a9858323dd442b26455bf9abefbe197f2708
chandanisake <chandanisake2002gmail.com>
2023-06-16 19:27:01 +0530

    first commit

---
+++ mi/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def kurnal(request):
+    return render(request,'kurnal.html')
+
---
+++ world/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sachin(request):
+    return render(request,'sachin.html')
